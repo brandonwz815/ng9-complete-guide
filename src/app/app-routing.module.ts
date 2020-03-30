5b321a4989bc9c5b6b429349ce13cc14e0ecd147
Brandon Zhu <bzhu@utraque.com>
2020-03-30 00:47:22 -0400

    155. Setting Up Routes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+
+const appRoutes: Routes = [
+    { path: '', redirectTo: '/recipes', pathMatch: 'full'},
+    { path: 'recipes', component: RecipesComponent },
+    { path: 'shopping-cart', component: ShoppingListService }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
