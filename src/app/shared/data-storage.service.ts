ceefd5b83a649515022a16d18069428ffbf794f7
Brandon Zhu <bzhu@utraque.com>
2020-04-12 23:06:31 -0400

    280. Storing Recipes

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { RecipeService } from '../recipes/recipe.service';
+import { Subject } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class DataStorageService {
+    error = new Subject();
+
+    constructor(private http: HttpClient,
+        private recipeService: RecipeService) { }
+
+    storeRecipes() {
+        const recipes = this.recipeService.getRecipes();
+        this.http.put<{ name: string }>(
+            'https://ng9-course-recipe-book.firebaseio.com/recipes.json',
+            recipes
+        ).subscribe(responseData => {
+            console.log(responseData)
+        });
+    }
+}
