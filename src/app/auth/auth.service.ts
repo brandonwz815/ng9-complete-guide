ab0a45ae37a70b9280a46bdfbdc3657fbbfee880
Brandon Zhu <bzhu@utraque.com>
2020-04-15 16:19:06 -0400

    298. Creating & Storing the User Data

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { throwError, Subject } from 'rxjs';
+
+import { User } from './user.model';
 
 export interface AuthResponseData {
     kind: string,
@@ -15,6 +17,9 @@ export interface AuthResponseData {
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+
+    user = new Subject<User>();
+
     constructor(private http: HttpClient) { }
 
     signup(email: string, password: string) {
@@ -25,7 +30,12 @@ export class AuthService {
                 password,
                 returnSecureToken: true
             }
-        ).pipe(catchError(this.handleError));
+        ).pipe(catchError(this.handleError), tap(resData => {this.handleAuthentication(
+            resData.expiresIn,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+        )}));
     }
 
     login(email: string, password: string) {
@@ -36,7 +46,12 @@ export class AuthService {
                 password,
                 returnSecureToken: true
             }
-        ).pipe(catchError(this.handleError));
+        ).pipe(catchError(this.handleError), tap(resData => {this.handleAuthentication(
+            resData.expiresIn,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+        )}));
     }
 
     private handleError(errorRes: HttpErrorResponse) {
@@ -56,6 +71,17 @@ export class AuthService {
                 break;
         }
         return throwError(errorMessage);
+    }
+
+    private handleAuthentication(email: string, userId: string, token: string, expiresIn: number ) {
+        const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+        const user = new User(
+            email,
+            userId,
+            token,
+            expirationDate
+        );
+        this.user.next(user);
     }
 
 }
