8e9e57774a476efbb58b89a07b421abf6f71343c
Brandon Zhu <bzhu@utraque.com>
2020-04-14 23:29:27 -0400

    294. Adding a Load Spinner and Error Handling Logic

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -9,6 +9,9 @@ import { AuthService } from './auth.service';
 })
 export class AuthComponent {
     isLoginMode = true;
+    isLoading = false;
+    error: string = null;
+    
 
     constructor(private authService: AuthService) { }
 
@@ -20,17 +23,23 @@ export class AuthComponent {
         if (form.valid) {
             const email = form.value.email;
             const password = form.value.password;
+            this.isLoading = true;
             if (this.isLoginMode) {
                 //...
             } else {
                 this.authService.signup(email, password).subscribe(resData => {
                     console.log(resData);
+                    this.isLoading = false;
                 }, error => {
                     console.log(error);
+                    this.error = 'An error occurred'
+                    this.isLoading = false;
                 });
             }
             form.reset();
+            return;
         }
+        return;
     }
 
 }
