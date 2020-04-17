224173faf4317e1dd73d5c8f1a762f1f84ca0a74
Brandon Zhu <bzhu@utraque.com>
2020-04-17 00:48:54 -0400

    305. Adding an Auth Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,18 +9,26 @@ import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.compon
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipesResolverService } from './recipes/recipes-resolver.service';
 import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const appRoutes: Routes = [
-    { path: '', redirectTo: '/recipes', pathMatch: 'full'},
-    { path: 'recipes', component: RecipesComponent, children: [
-        { path: '', component: RecipeStartComponent },
-        { path: 'new', component: RecipeEditComponent },
-        { path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService] },
-        { path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService] },
-    ] },
-    { path: 'shopping-list', component: ShoppingListComponent, children: [
-        { path: ':id', component: ShoppingEditComponent }
-    ] },
+    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+    {
+        path: 'recipes', 
+        component: RecipesComponent, 
+        canActivate: [AuthGuard],
+        children: [
+            { path: '', component: RecipeStartComponent },
+            { path: 'new', component: RecipeEditComponent },
+            { path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService] },
+            { path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService] },
+        ]
+    },
+    {
+        path: 'shopping-list', component: ShoppingListComponent, children: [
+            { path: ':id', component: ShoppingEditComponent }
+        ]
+    },
     { path: 'auth', component: AuthComponent }
 ];
 
@@ -28,4 +36,4 @@ const appRoutes: Routes = [
     imports: [RouterModule.forRoot(appRoutes)],
     exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
