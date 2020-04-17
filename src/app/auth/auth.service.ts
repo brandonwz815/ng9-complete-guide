9bb1502bb363b4ab0ec343b9ea977e93048f17c7
Brandon Zhu <bzhu@utraque.com>
2020-04-17 00:00:03 -0400

    303. Adding Auto-Login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -49,11 +49,34 @@ export class AuthService {
                 returnSecureToken: true
             }
         ).pipe(catchError(this.handleError), tap(resData => {this.handleAuthentication(
-            resData.expiresIn,
+            resData.email,
             resData.localId,
             resData.idToken,
             +resData.expiresIn
         )}));
+    }
+
+    autoLogin() {
+        const userData: {
+            email: string;
+            id: string;
+            _token: string;
+            _tokenExpirationDate: string;
+        } = JSON.parse(localStorage.getItem('userData'));
+        if(!userData) {
+            return;
+        }
+
+        const loadedUser = new User(
+            userData.email,
+            userData.id,
+            userData._token,
+            new Date(userData._tokenExpirationDate)
+        );
+
+        if(loadedUser.token) {
+            this.user.next(loadedUser);
+        }
     }
 
     logout() {
@@ -89,6 +112,7 @@ export class AuthService {
             expirationDate
         );
         this.user.next(user);
+        localStorage.setItem('userData', JSON.stringify(user));
     }
 
 }
