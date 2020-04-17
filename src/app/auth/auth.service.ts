e1638bfbcb8c3451c8841ba9ffed198eed93bc33
Brandon Zhu <bzhu@utraque.com>
2020-04-16 23:35:05 -0400

    302. Adding Logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError, tap } from 'rxjs/operators';
 import { throwError, BehaviorSubject } from 'rxjs';
+import { Router } from '@angular/router';
 
 import { User } from './user.model';
 
@@ -20,7 +21,8 @@ export class AuthService {
 
     user = new BehaviorSubject<User>(null);
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient,
+                private router: Router) { }
 
     signup(email: string, password: string) {
         return this.http.post<AuthResponseData>(
@@ -52,6 +54,11 @@ export class AuthService {
             resData.idToken,
             +resData.expiresIn
         )}));
+    }
+
+    logout() {
+        this.user.next(null);
+        this.router.navigate(['/auth']);
     }
 
     private handleError(errorRes: HttpErrorResponse) {
