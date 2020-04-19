bf941dd3e5db81ac96c3cb98d0c6b730f963c14a
Brandon Zhu <bzhu@utraque.com>
2020-04-19 00:59:57 -0400

    326. Understanding the Shared Modules

--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from "@angular/core";
-import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { ShoppingListComponent } from './shopping-list.component';
 import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
     declarations: [
@@ -12,11 +12,11 @@ import { RouterModule } from '@angular/router';
         ShoppingEditComponent,
     ],
     imports: [
-        CommonModule,
         FormsModule,
         RouterModule.forChild([
             { path: 'shopping-list', component: ShoppingListComponent }
         ]),
+        SharedModule
     ],
     exports: [
         ShoppingListComponent,
