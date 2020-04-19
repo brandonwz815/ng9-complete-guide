f253e213fe09689ab3ae92f49d8b632f2801caf7
Brandon Zhu <bzhu@utraque.com>
2020-04-19 13:34:48 -0400

    331. More Lazy Loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { listLazyRoutes } from '@angular/compiler/src/aot/lazy_routes';
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-    // { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule' },
-    { path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(m=>m.RecipesModule) },
+    { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule' },
+    // { path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(m=>m.RecipesModule) },    
+    { path: 'shopping-list', loadChildren: './shopping-list/shopping-list.module#ShoppingListModule' },
+    // { path: 'shopping-list', loadChildren: () => import('./shopping-list/shopping-list.module').then(m=>m.ShoppingListModule) }
+    { path: 'auth', loadChildren: './auth/auth.module#AuthModule' }
 ];
 
 @NgModule({
