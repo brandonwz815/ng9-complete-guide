86ecb142a20535396c4f8ba9cb0859b95378f1f3
Brandon Zhu <bzhu@utraque.com>
2020-04-19 21:30:14 -0400

    350. Selecting State

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -13,6 +13,8 @@ const initialState = {
 export function shoppingListReducer(state = initialState, action: ShoppingListActions.AddIngredient) {
     switch(action.type) {
         case ShoppingListActions.ADD_INGREDIENT:
-            return {...state, ingredients:[...state.ingredients, action.payload]}
+            return {...state, ingredients:[...state.ingredients, action.payload]};
+        default:
+            return state;
     }
 }
