8031fdfe03d60166daae61dfe0cca7d87ec6cb6b
Brandon Zhu <bzhu@utraque.com>
2020-04-26 02:31:33 -0400

    362. Auth Finished (For Now...)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
 
 import { DataStorageService } from '../shared/data-storage.service';
 import { AuthService } from '../auth/auth.service';
-import { Subscription } from 'rxjs';
+import * as fromApp from '../store/app.reducer';
 
 @Component({
   selector: 'app-header',
@@ -14,14 +17,18 @@ export class HeaderComponent implements OnInit, OnDestroy {
   private userSub: Subscription;
 
   constructor(private dataStorageService: DataStorageService,
-              private authService: AuthService) {}
+    private authService: AuthService,
+    private store: Store<fromApp.AppState>) { }
 
   ngOnInit() {
-    this.userSub = this.authService.user.subscribe(user => {
-      this.isAuthenticated = !!user;
-      console.log(!user);
-      console.log(!!user)
-    });
+    this.userSub = this.store
+      .select('auth')
+      .pipe(map(appState => appState.user))
+      .subscribe(user => {
+        this.isAuthenticated = !!user;
+        console.log(!user);
+        console.log(!!user)
+      });
   }
 
   ngOnDestroy() {
