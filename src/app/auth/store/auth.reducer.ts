f6ed4950e4222da84650d478c2aa7c834a5ac936
Brandon Zhu <bzhu@utraque.com>
2020-04-26 01:42:06 -0400

    360. Setting Up Auth Reducer & Actions

--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -1,4 +1,5 @@
 import { User } from '../user.model';
+import * as AuthActions from './auth.actions';
 
 
 export interface State {
@@ -9,10 +10,21 @@ const initialState = {
     user: null
 }
 
-export function authReducer(state = initialState, action) {
-    switch(action.type) {
-        // case
-        default: 
+export function authReducer(state = initialState, action: AuthActions.AuthActions) {
+    switch (action.type) {
+        case AuthActions.LOGIN:
+            return {
+                ...state,
+                user: new User(
+                    action.payload.email,
+                    action.payload.userId,
+                    action.payload.token,
+                    action.payload.expirationDate
+                )
+            };
+        case AuthActions.LOGOUT:
+            return initialState;
+        default:
             return state;
     }
 }
