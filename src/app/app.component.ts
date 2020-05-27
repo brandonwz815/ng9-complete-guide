47119a9dba26f3db6e71851ff74e9acb87bd1d5c
Brandon Zhu <bzhu@utraque.com>
2020-05-27 00:34:55 -0400

    390. Adding Angular Universal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { Store } from '@ngrx/store';
 
 import { AuthService } from './auth/auth.service';
@@ -14,12 +15,16 @@ import * as AuthActions from './auth/store/auth.actions';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private store: Store<fromAll.AppState>,
-              private loggingService: LoggingService) {}
+  constructor(
+    private store: Store<fromAll.AppState>,
+    private loggingService: LoggingService,
+    @Inject(PLATFORM_ID) private platformId
+  ) { }
 
   ngOnInit() {
-    // this.authService.autoLogin();
-    this.store.dispatch(new AuthActions.AutoLogin());
+    if (isPlatformBrowser(this.platformId)) {
+      this.store.dispatch(new AuthActions.AutoLogin());
+    }
     this.loggingService.printLog('Hello from AppComponent.ngOnInit()')
   }
 }
